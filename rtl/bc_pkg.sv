f2ceec3a65b6bd2adafe163dbbfe2cff7e053a18
SinDrag0n <vladislavbelyaev04@gmail.com>
2026-02-11 22:13:42 +0200

    Feat[dev-CDB] Added project package file

---
+++ rtl/bc_pkg.sv
@@ -0,0 +1,36 @@
+package bc_pkg;
+
+parameter int unsigned DATA_WIDTH    = 32;
+parameter int unsigned INSTR_WIDTH   = 32;
+
+parameter int unsigned RF_REGS_NUM   = 32;
+parameter int unsigned RF_ADDR_WIDTH = 32;
+
+parameter int unsigned CDB_USERS     = 2;
+
+parameter int unsigned RS_NUM        = 2;
+
+parameter int unsigned OP_ADDR       = $clog2( CDB_USERS );
+
+
+
+typedef struct packed {
+  logic [RF_ADDR_WIDTH - 1:0] rs_rd_addr;
+  logic [   DATA_WIDTH - 1:0] rs_rd_data;
+  logic                       cdb_valid;
+} rs_cdb_port_t;
+
+typedef struct packed {
+  logic [RF_ADDR_WIDTH - 1:0] rob_rd_addr;
+  logic [   DATA_WIDTH - 1:0] rob_rd_data;
+  logic                       cdb_valid;
+} rob_cdb_port_t;
+
+typedef struct packed {
+  logic [RF_ADDR_WIDTH - 1:0] func_block_wr_addr;
+  logic [   DATA_WIDTH - 1:0] func_block_wr_data;
+  logic                       cdb_valid;
+} func_block_cdb_port_t;
+
+
+endpackage
